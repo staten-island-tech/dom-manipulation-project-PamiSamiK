eefc307fae5a31b101504deaf703ac0d0db6bb66
PamiSamiK <144476939+PamiSamiK@users.noreply.github.com>
2023-10-25 21:36:33 -0400

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,42 @@
+const DomSelectors = {
+    form: document.getElementById('p'),
+    nameInput: document.getElementById(`a`),
+    hobbyInput: document.getElementById('m'),
+    addDataButton: document.getElementById('i'),
+    dataList: document.getElementById('s'),
+}
+
+function createDataObject(name, hobby) {
+    return { name, hobby};
+  }
+
+  function injectDataObject(dataObject) {
+    const dataItem = document.createElement('div');
+    dataItem.innerHTML = `<strong>Name:</strong> ${dataObject.name}, <strong>Email:</strong> ${dataObject.email}
+    <button class="remove-data">Remove</button>`;
+    DOMSelectors.dataList.appendChild(dataItem);
+  }
+
+  function clearInputFields() {
+    DOMSelectors.nameInput.value = '';
+    DOMSelectors.hobbyInput.value = '';
+  }
+
+  function removeDataObject(event) {
+    if (event.target.classList.contains('remove-data')) {
+      event.target.parentElement.remove();
+    }
+  }
+
+  DOMSelectors.addDataButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const name = DOMSelectors.nameInput.value;
+    const hobby = DOMSelectors.emailInput.value;
+    if (name && hobby) {
+      const dataObject = createDataObject(name, hobby);
+      injectDataObject(dataObject);
+      clearInputFields();
+    }
+  });
+
+  DOMSelectors.dataList.addEventListener('click', removeDataObject);
