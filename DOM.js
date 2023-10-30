3c2cbe20f254420ce355e860bb80acdc82bf7b29
PamiSamiK <144476939+PamiSamiK@users.noreply.github.com>
2023-10-29 22:24:23 -0400

    rtek7

--- DOM.js
+++ DOM.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const DOMSelectors = {
   form: document.getElementById('myForm'),
   nameInput: document.getElementById('name'),
@@ -34,49 +33,3 @@ DOMSelectors.form.addEventListener('submit', function (event) {
   DOMSelectors.nameInput.value = '';
   DOMSelectors.hobbyInput.value = '';
 });
-=======
-const DomSelectors = {
-    form: document.getElementById('p'),
-    nameInput: document.getElementById(`a`),
-    hobbyInput: document.getElementById('m'),
-    addDataButton: document.getElementById('i'),
-    dataList: document.getElementById('s'),
-}
-
-function createDataObject(name, hobby) {
-    return { name, hobby};
-  }
-
-  function injectDataObject(dataObject) {
-    const dataItem = document.createElement('div');
-    dataItem.innerHTML = `<strong>Name:</strong> ${dataObject.name}, <strong>hobby:</strong> ${dataObject.hobby}
-    <button class="remove-data">Remove</button>`;
-    DOMSelectors.dataList.appendChild(dataItem);
-  }
-
-  function clearInputFields() {
-    DOMSelectors.nameInput.value = 'x';
-    DOMSelectors.hobbyInput.value = 'y';
-  }
-
-  function removeDataObject(event) {
-    if (event.target.classList.contains('remove-data')) {
-      event.target.parentElement.remove();
-    }
-  }
-
-  DOMSelectors.addDataButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    const name = DOMSelectors.nameInput.value;
-    const hobby = DOMSelectors.emailInput.value;
-    if (name && hobby) {
-      const dataObject = createDataObject(name, hobby);
-      injectDataObject(dataObject);
-      clearInputFields();
-    }
-  });
-
-  DOMSelectors.dataList.addEventListener('click', removeDataObject);
-
-
->>>>>>> d2dd754c21e9e08e67bafad674867ea5b4540c29
