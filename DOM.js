561007eb25fee1bb73b4926dc227f9794a796a22
PamiSamiK <144476939+PamiSamiK@users.noreply.github.com>
2023-11-07 13:00:23 -0500

    FINALLY

--- DOM.js
+++ DOM.js
@@ -1,32 +1,45 @@
 const DOMSelectors = {
   form: document.querySelector('#myForm'),
-  artistInput: document.querySelector('#artist'),
-  genreInput: document.querySelector('#genre'),
-  albumInput: document.querySelector('#album'),
-  outputDiv: document.querySelector('#output'),
+  artist: document.querySelector('#artist'),
+  genre: document.querySelector('#genre'),
+  album: document.querySelector('#album'),
+  image: document.querySelector('#image'),
+  gallery: document.querySelector('#gallery'),
 };
-function create(){
-  
+
+function injectCard(wholeAlbum) {
+  const cardDiv = document.createElement("div");
+  cardDiv.className = "card";
+
+  cardDiv.innerHTML = `
+    <h2>${wholeAlbum.artist}</h2>
+    <h3>${wholeAlbum.genre}</h3>
+    <h3>${wholeAlbum.album}</h3>
+    <img src="${wholeAlbum.image}" alt="${wholeAlbum.album}" class="album-image">
+    <button class="remove-button">Remove</button>
+  `;
+
+  DOMSelectors.gallery.appendChild(cardDiv);
+
+  const removeButton = cardDiv.querySelector(".remove-button");
+  removeButton.addEventListener("click", function() {
+    cardDiv.remove(); 
+  });
 }
-function inject() {
-  DOMSelectors.outputDiv.insertAdjacentHTML(
-    "beforeend",
-    `
-    <div class="box">
-    <h2>${DOMSelectors.artistInput}</h2>
-    <h3>${DOMSelectors.genreInput}</h3>
-    <img src="${DOMSelectors.albumInput}"alt="">
-    </div>`
-)} 
-DOMSelectors.form.addEventListener('submit', function (event) {
+
+DOMSelectors.form.addEventListener("submit", function(event){
   event.preventDefault();
-  create() // Prevent the default form submission behavior
 
-  // Get user input from form
+  const wholeAlbum = {
+    artist: DOMSelectors.artist.value,
+    genre: DOMSelectors.genre.value,
+    album: DOMSelectors.album.value,
+    image: DOMSelectors.image.value,
+  };
 
+  injectCard(wholeAlbum);
+});
 
-  inject()
-});  
 
 
 
